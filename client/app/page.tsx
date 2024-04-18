8ff7e6bc6101abe3089d39780d15768d3eeaf3fd
amartripathi0 <theamartripathi@gmail.com>
2024-04-18 13:10:15 +0530

    added:init nextjs boilerplate

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <main>
+
+      <div className="grid  grid-cols-12 h-screen w-screen ">
+        <div className="col-span-3"></div>
+        <div className="col-span-6 border-x border-gray-700"></div>
+        <div className="col-span-3"></div>
+      </div>
+    </main>
+  );
+}
